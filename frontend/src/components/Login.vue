a66bf0c0ed18bf01c8cfb4266faa08d8a8de0e62
Stefan <stefan.popescu@student.fontys.nl>
2024-10-10 08:14:24 +0200

    frontend setup

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <h2>Login</h2>
+      <form>
+        <input v-model="username" placeholder="Username" />
+        <input v-model="password" type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
