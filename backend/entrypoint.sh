6c0cdf869f8c1490c7e6fdf50d7f3e7416efc7c1
Stefan <stefan.popescu@student.fontys.nl>
2024-12-09 13:55:59 +0100

    Adjusted the compose file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,9 +7,15 @@ python manage.py migrate
 # Create a default superuser if it doesn't exist
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
+import os
+
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
+if not User.objects.filter(username=os.getenv('DJANGO_SUPERUSER_USERNAME')).exists():
+    User.objects.create_superuser(
+        username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
+        email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+        password=os.getenv('DJANGO_SUPERUSER_PASSWORD')
+    )
 EOF
 
 # Start the server
